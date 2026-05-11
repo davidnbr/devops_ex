0b8b46364b94d99e5750395a64b8b46e92b80bde
David <davidnbr_98@hotmail.com>
2026-05-10 20:50:57 -0500

    chore: add initial network setup

---
+++ terraform/modules/network/variables.tf
@@ -0,0 +1,26 @@
+variable "name" {
+  description = "Name prefix for network resources."
+  type        = string
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC."
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "availability_zones" {
+  description = "Availability zones used for public subnets."
+  type        = list(string)
+}
+
+variable "public_subnet_cidrs" {
+  description = "CIDR blocks for public subnets."
+  type        = list(string)
+}
+
+variable "tags" {
+  description = "Tags applied to all supported resources."
+  type        = map(string)
+  default     = {}
+}
